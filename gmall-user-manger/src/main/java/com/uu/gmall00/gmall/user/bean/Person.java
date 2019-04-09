22c171513954894ae94672db96e63b6ae65d39a1
陈友友 <782121021@qq.com>
2019-04-09 22:40:45 +0800

    update person toString

--- gmall-user-manger/src/main/java/com/uu/gmall00/gmall/user/bean/Person.java
+++ gmall-user-manger/src/main/java/com/uu/gmall00/gmall/user/bean/Person.java
@@ -4,4 +4,12 @@ public class Person {
 
     String name;
     Integer age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
