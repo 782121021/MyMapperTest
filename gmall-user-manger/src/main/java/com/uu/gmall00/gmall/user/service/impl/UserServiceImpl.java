eedb2c01b626f89f78ca570cbf1771a97dafaa41
陈友友 <782121021@qq.com>
2019-04-09 23:25:26 +0800

    修改方法

--- gmall-user-manger/src/main/java/com/uu/gmall00/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user-manger/src/main/java/com/uu/gmall00/gmall/user/service/impl/UserServiceImpl.java
@@ -29,7 +29,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void updateUser(UserInfo userInfo) {
-
+        userInfoMapper.updateByPrimaryKey(userInfo);
     }
 
     @Override
