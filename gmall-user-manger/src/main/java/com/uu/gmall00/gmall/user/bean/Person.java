75478676ee1a4a80eb03fda8e24ecf4684df87ac
陈友友 <782121021@qq.com>
2019-04-09 22:50:08 +0800

    update 2

--- gmall-user-manger/src/main/java/com/uu/gmall00/gmall/user/bean/Person.java
+++ gmall-user-manger/src/main/java/com/uu/gmall00/gmall/user/bean/Person.java
@@ -7,6 +7,21 @@ public class Person {
 
     public Person(){}
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     @Override
     public String toString() {
